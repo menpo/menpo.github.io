a544443866e07096705d64453cc180d9dd5bc5a1
James Booth <jabooth@gmail.com>
2016-07-04 14:14:12 +0100

    update ./build.sh to retain verification file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-find . -maxdepth 1 ! -name '.gitignore' ! -name 'build.sh' ! -name 'src' ! -name '.git' ! -name '.' ! -name 'CNAME' -exec rm -rf {} +
+find . -maxdepth 1 ! -name 'googlec43c65618a70e0c8.html' ! -name '.gitignore' ! -name 'build.sh' ! -name 'src' ! -name '.git' ! -name '.' ! -name 'CNAME' -exec rm -rf {} +
 cd src
 gitbook install
 gitbook build
